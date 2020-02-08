260eb60fac4b0b400ff12570a9e0df7893d225f8
CarolEnache <enache.carol@gmail.com>
2020-02-08 18:50:46 +0000

    handle actions

---
+++ src/redux/reducers.js
@@ -0,0 +1,42 @@
+import {
+  ADD_TODO,
+  TOGGLE_TODO,
+  SET_VISIBILITY_FILTER,
+  VisibilityFilters
+} from './actions';
+
+const initialState = {
+  visibilityFilters: VisibilityFilters.SHOW_ALL,
+  todos: []
+};
+
+function todoApp(state = initialState, action) {
+  switch (action.type) {
+    case SET_VISIBILITY_FILTER:
+      return Object.assign({}, state, {
+        visibilityFilters: action.filter
+      });
+    case ADD_TODO:
+      return Object.assign({}, state, {
+        todos: [
+          ...state.todos,
+          {
+            text: action.text,
+            completed: false
+          }
+        ]
+      });
+    case TOGGLE_TODO:
+      return Object.assign({}, state, {
+        todos: state.todos.map((todo, index) => {
+          if (index === action.index) {
+            return Object.assign({}, todo, {
+              completed: !todo.completed
+            });
+          }
+        })
+      });
+    default:
+      return state;
+  }
+}
