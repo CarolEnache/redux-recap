4ee2885c9a77fb7b4c7b2bdfddd54a268f5ab82a
CarolEnache <enache.carol@gmail.com>
2020-02-08 20:22:08 +0000

    Splitting Reduces

--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -4,39 +4,51 @@ import {
   SET_VISIBILITY_FILTER,
   VisibilityFilters
 } from './actions';
+import { combineReducers } from 'redux';
 
-const initialState = {
-  visibilityFilters: VisibilityFilters.SHOW_ALL,
-  todos: []
-};
+const { SHOW_ALL } = VisibilityFilters;
 
-function todoApp(state = initialState, action) {
+/**
+Note that todos also accepts state—but state is an array!
+Now todoApp gives todos just a slice of the state to manage, and todos knows how to update just that slice.
+This is called reducer composition, and it's the fundamental pattern of building Redux apps.
+*/
+
+function todos(state = [], action) {
   switch (action.type) {
-    case SET_VISIBILITY_FILTER:
-      return Object.assign({}, state, {
-        visibilityFilters: action.filter
-      });
     case ADD_TODO:
-      return Object.assign({}, state, {
-        todos: [
-          ...state.todos,
-          {
-            text: action.text,
-            completed: false
-          }
-        ]
-      });
+      return [
+        ...state.todos,
+        {
+          text: action.text,
+          completed: false
+        }
+      ];
     case TOGGLE_TODO:
-      return Object.assign({}, state, {
-        todos: state.todos.map((todo, index) => {
-          if (index === action.index) {
-            return Object.assign({}, todo, {
-              completed: !todo.completed
-            });
-          }
-        })
+      return state.todos.map((todo, index) => {
+        if (index === action.index) {
+          return Object.assign({}, todo, {
+            completed: !todo.completed
+          });
+        }
       });
     default:
       return state;
   }
 }
+
+function visibilityFilters(state = SHOW_ALL, action) {
+  switch (action.type) {
+    case SET_VISIBILITY_FILTER:
+      return action.filter;
+    default:
+      return state;
+  }
+}
+
+const todoApp = combineReducers({
+  visibilityFilters,
+  todos
+});
+
+export default todoApp;
