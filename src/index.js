8cca1abf6d074f5bd9d87c6cf766b6d42dcec963
CarolEnache <enache.carol@gmail.com>
2020-02-09 22:20:03 +0000

    Redux usage with React Router

--- src/index.js
+++ src/index.js
@@ -1,15 +1,9 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import rootReducer from './redux/reducers/index';
-import App from './App';
+import Root from './components/Root';
 
 const store = createStore(rootReducer);
 
-render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root')
-);
+render(<Root store={store} />, document.getElementById('root'));
